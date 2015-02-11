98e8fc0d629dec71c18563fc301b0a9ce301b2b7
Ari Telesman <futbol3737@yahoo.com>
2015-02-11 18:12:10 -0500

    Created navigation view and drew triangle in Manhattan

--- app/src/main/java/com/osucse/wayfinding_osu_capstone/MainActivity.java
+++ app/src/main/java/com/osucse/wayfinding_osu_capstone/MainActivity.java
@@ -1,5 +1,5 @@
 package com.osucse.wayfinding_osu_capstone;
-
+/**
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -35,5 +35,54 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+}
+*/
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+public class MainActivity extends FragmentActivity {
+    private static final LatLng LOWER_MANHATTAN = new LatLng(40.722543,
+            -73.998585);
+    private static final LatLng TIMES_SQUARE = new LatLng(40.7577, -73.9857);
+    private static final LatLng BROOKLYN_BRIDGE = new LatLng(40.7057, -73.9964);
+
+    private GoogleMap googleMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        // check if we have got the googleMap already
+        if (googleMap == null) {
+            googleMap = ((SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map)).getMap();
+            if (googleMap != null) {
+                addLines();
+            }
+        }
+    }
+
+    private void addLines() {
+
+        googleMap
+                .addPolyline((new PolylineOptions())
+                        .add(TIMES_SQUARE, BROOKLYN_BRIDGE, LOWER_MANHATTAN,
+                                TIMES_SQUARE).width(5).color(Color.BLUE)
+                        .geodesic(true));
+        // move camera to zoom on map
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LOWER_MANHATTAN,
+                13));
     }
 }
