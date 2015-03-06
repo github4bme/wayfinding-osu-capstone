0970a7ea47d51e24c13b915382a865850f833ae9
Tommy Forte <forte.39@osu.edu>
2015-03-05 21:37:34 -0500

    A better way to handle ArrayList's of Location and their copies.

--- app/src/main/java/com/osucse/wayfinding_osu_capstone/StartUpTasks.java
+++ app/src/main/java/com/osucse/wayfinding_osu_capstone/StartUpTasks.java
@@ -19,8 +19,10 @@ public class StartUpTasks {
     // server url
     private static final String URL = "http://54.200.238.22:9000/";
 
-    // location data
+    // location data that is copied from the api server
     private static LocationCollection LOCATION_COLLECTION = null;
+
+    private static ArrayList<Location> LOCATION_LIST = null;
 
     /**
      * getLocationCollectionFromServer will attempt to get a connection
@@ -38,36 +40,47 @@ public class StartUpTasks {
             RestTemplate restTemplate = new RestTemplate();
             restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
             LOCATION_COLLECTION = restTemplate.getForObject(url, LocationCollection.class);
+            LOCATION_LIST = makeLocationList(LOCATION_COLLECTION);
         } catch (Exception e) {
             Log.e("LocationList", e.getMessage(), e);
         }
         return LOCATION_COLLECTION;
     }
 
+
+
     /**
-     * cloneLocationCollection (couldn't make a deep copy because there are no constructors
-     * allowed on the app side)
-     * @return a shallow copy of LOCATION_COLLECTION
+     * cloneLocationCollection
+     * @return a shallow copy of LOCATION_LIST
      */
     public static ArrayList<Location> cloneLocationCollection () {
+        return new ArrayList<Location>(LOCATION_LIST);
+    }
+
+    /**
+     * Processes the LOCATION_COLLECTION by removing null locations, and sorting the
+     * remaining locations. The result is returned
+     * @param locationCollection a LocationCollection to process
+     * @return a processed ArrayList of the LocationCollection data
+     */
+    private static ArrayList<Location> makeLocationList (LocationCollection locationCollection) {
 
         // initialize a new array
-        ArrayList<Location> clone = new ArrayList<Location>(LOCATION_COLLECTION.getLocations().size());
+        ArrayList<Location> temp = new ArrayList<Location>(locationCollection.getLocations().size());
 
         // removes null location data while copying pointers
         for(Location l : LOCATION_COLLECTION.getLocations())
         {
             if(l.getName() != null) {
-                clone.add(l);
+                LOCATION_LIST.add(l);
             }
         }
 
         // sorts the LocationCollection by name (see added compareTo part of the location class)
-        Collections.sort(clone);
+        Collections.sort(LOCATION_LIST);
 
-        return clone;
+        return temp;
     }
-
 
 
 }
