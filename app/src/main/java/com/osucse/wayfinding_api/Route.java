5a70a66076e8a7a7da450d2ed586aeb072fe5931
Ari Telesman <futbol3737@yahoo.com>
2015-03-08 14:10:39 -0400

    Route plotted on map. Back button currently bugged on this activity

---
+++ app/src/main/java/com/osucse/wayfinding_api/Route.java
@@ -0,0 +1,16 @@
+package com.osucse.wayfinding_api;
+
+import java.util.List;
+
+public class Route {
+    private List<Node> route;
+
+//    public Route(List<Node> route){
+//        this.route = route;
+//    }
+
+    public List<Node> getRoute(){
+        return route;
+    }
+
+}
--- app/src/main/java/com/osucse/wayfinding_osu_capstone/DisplayMapActivity.java
+++ app/src/main/java/com/osucse/wayfinding_osu_capstone/DisplayMapActivity.java
@@ -3,102 +3,113 @@ package com.osucse.wayfinding_osu_capstone;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.widget.TextView;
-
+import android.util.Log;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolylineOptions;
 import java.util.*;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+import com.osucse.wayfinding_api.*;
+import com.osucse.utilities.Coordinate;
 
 
 public class DisplayMapActivity extends FragmentActivity {
-    private static final LatLng LOWER_MANHATTAN = new LatLng(40.722543,
-            -73.998585);
-    private static final LatLng TIMES_SQUARE = new LatLng(40.7577, -73.9857);
-    private static final LatLng BROOKLYN_BRIDGE = new LatLng(40.7057, -73.9964);
-    //private static final LatLng STILLMAN_HALL = new LatLng(	40.0018248, -083.0110277);
-    //private static final LatLng DEST = new LatLng(40.0018227, -083.0110479);
-    //private static final LatLng NEXT = new LatLng(40.0018760, -083.0110638);
-    //private static final LatLng NEXT = new LatLng(40.0018100, -083.0110455);
-    //private static final LatLng NEXT1 = new LatLng(40.0017233, -083.0109211);
 
-    //Curved line
-    //    private static final LatLng DEST = new LatLng(39.9960940, -083.0140383);
-    //    private static final LatLng NEXT = new LatLng(39.9960905, -083.0140724);
-    //    private static final LatLng NEXT1 = new LatLng(39.9960909, -083.0141052);
-    //    private static final LatLng NEXT2 = new LatLng(39.9960955, -083.0141336);
+    private static final String URL = "http://54.200.238.22:9000/";
 
-    private static final LatLng DEST = new LatLng(39.9986444, -083.0150867);
-    private static final LatLng NEXT = new LatLng(39.9985652, -083.0151295);
-    private static final LatLng NEXT1 = new LatLng(39.9984717, -083.0151624);
-    private static final LatLng NEXT2 = new LatLng(39.9983703, -083.0151790);
-    private static final LatLng NEXT3 = new LatLng(39.9983301, -083.0151664);
-
+//    private static final LatLng DEST = new LatLng(39.9986444, -083.0150867);
+//    private static final LatLng NEXT = new LatLng(39.9985652, -083.0151295);
+//    private static final LatLng NEXT1 = new LatLng(39.9984717, -083.0151624);
+//    private static final LatLng NEXT2 = new LatLng(39.9983703, -083.0151790);
+//    private static final LatLng NEXT3 = new LatLng(39.9983301, -083.0151664);
+    private static String startLocation = "";
+    private static String endLocation = "";
     private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_map);
-
         Intent intent = getIntent();
-        String startLocation = intent.getStringExtra(SelectDestinationLocation.SOURCE_LOCATION);
-        String endLocation = intent.getStringExtra(SelectDestinationLocation.DESTINATION_LOCATION);
+        startLocation = intent.getStringExtra(SelectDestinationLocation.SOURCE_LOCATION);
+        endLocation = intent.getStringExtra(SelectDestinationLocation.DESTINATION_LOCATION);
 
         //TextView textView = new TextView(this);
         TextView startLocationDisplay = (TextView) findViewById(R.id.start_location_display);
         startLocationDisplay.setTextSize(20);
         startLocationDisplay.setText("starting id: " + startLocation + " ending id:" + endLocation);
         //setContentView(textView);
-
+        new HttpRequestTask().execute();
         setUpMapIfNeeded();
     }
 
     private void setUpMapIfNeeded() {
-        List<LatLng> path = new ArrayList<LatLng>();
-        /*path.add(LOWER_MANHATTAN);
-        path.add(TIMES_SQUARE);
-        path.add(BROOKLYN_BRIDGE);
-        path.add(LOWER_MANHATTAN);*/
-        path.add(DEST);
-        path.add(NEXT);
-        path.add(NEXT1);
-        path.add(NEXT2);
-        path.add(NEXT3);
         // check if we have got the googleMap already
         if (googleMap == null) {
             googleMap = ((SupportMapFragment) getSupportFragmentManager()
                     .findFragmentById(R.id.map)).getMap();
             if (googleMap != null) {
-                addLines(path);
                 googleMap.setMyLocationEnabled(true);
             }
         }
     }
 
-    private void addLines(List<LatLng> path) {
-        int pathSize = path.size();
+    private class HttpRequestTask extends AsyncTask<Void, Void, Route> {
+        @Override
+        protected Route doInBackground(Void... params) {
+            try {
+
+                final String url = URL + "generateRoute?from=" + startLocation + "&to=" + endLocation;
+                RestTemplate restTemplate = new RestTemplate();
+                restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                Route collection = restTemplate.getForObject(url, Route.class);
+                return collection;
+            } catch (Exception e) {
+                Log.e("Route", e.getMessage(), e);
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Route collection) {
+            final List<Node> routePoints = collection.getRoute();
+            plotRoute(routePoints);
+        }
+
+    }
+
+
+    /**
+     * Plots the route on the map
+     * @param route
+     */
+    private void plotRoute(List<Node> route) {
+        int pathSize = route.size();
         int current =0;
         LatLng start = null;
         LatLng end = null;
         for (int i =1; i < pathSize; i++){
             if (start == null) {
-                start = path.get(current);
+                start = new LatLng(route.get(current).getCoordinate().getLatitude(), route.get(current).getCoordinate().getLongitude());
             }else{
                 start = end;
             }
             current++;
-            end = path.get(current);
+            end = new LatLng(route.get(current).getCoordinate().getLatitude(), route.get(current).getCoordinate().getLongitude());
             googleMap
                     .addPolyline((new PolylineOptions())
                             .add(start, end).width(5).color(Color.BLUE)
                             .geodesic(true));
             // move camera to zoom on map
             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(start,
-                    18));
+                    17));
         }
     }
 }
