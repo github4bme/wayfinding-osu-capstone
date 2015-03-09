1ea7ed506c67a0f8265deb05fc137ab1daaa91dc
joshosu999 <trick.8@osu.edu>
2015-03-09 13:51:06 -0400

    Map changed to monitor location change and print coordinates to the screen.

--- app/src/main/java/com/osucse/wayfinding_osu_capstone/DisplayMapActivity.java
+++ app/src/main/java/com/osucse/wayfinding_osu_capstone/DisplayMapActivity.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 
 // Josh added these imports for updating user's current location
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.Marker;
@@ -20,18 +22,42 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.text.DateFormat;
 import java.util.*;
 
 
-public class DisplayMapActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
+public class DisplayMapActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener, OnMapReadyCallback {
     private static final LatLng DEST = new LatLng(39.9986444, -083.0150867);
     private static final LatLng NEXT = new LatLng(39.9985652, -083.0151295);
     private static final LatLng NEXT1 = new LatLng(39.9984717, -083.0151624);
     private static final LatLng NEXT2 = new LatLng(39.9983703, -083.0151790);
     private static final LatLng NEXT3 = new LatLng(39.9983301, -083.0151664);
 
+    private static final String REQUESTING_LOCATION_UPDATES_KEY = "com.osucse.wayfinding_osu_capstone.DisplayMapActivity.requestingLocationUpdatesKey";
+    private static final String LOCATION_KEY = "com.osucse.wayfinding_osu_capstone.DisplayMapActivity.locationKey";
+    private static final String LAST_UPDATED_TIME_STRING_KEY = "com.osucse.wayfinding_osu_capstone.DisplayMapActivity.lastUpdatedTimeStringKey";
+
     protected GoogleMap ourMap;
     protected GoogleApiClient mGoogleApiClient;
+    protected LocationRequest mLocationRequest;
+
+
+
+    /****** to be deleted
+     *
+     */
+
+    protected TextView latDisplay;
+    protected TextView lngDisplay;
+    protected TextView timeDisplay;
+
+    protected android.location.Location mCurrentLocation;
+    protected String mLastUpdateTime;
+    /**/
+    /** */
+    //?????
+    protected boolean mRequestingLocationUpdates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +72,31 @@ public class DisplayMapActivity extends FragmentActivity implements GoogleApiCli
         startLocationDisplay.setTextSize(20);
         startLocationDisplay.setText("starting id: " + startLocation + " ending id:" + endLocation);
 
+
+        /****** to be deleted
+         *
+         */
+        latDisplay = (TextView) findViewById(R.id.lat_display);
+        latDisplay.setTextSize(20);
+        latDisplay.setText("lat: ");
+
+
+        lngDisplay = (TextView) findViewById(R.id.lng_display);
+        lngDisplay.setTextSize(20);
+        lngDisplay.setText("lng: ");
+
+        timeDisplay = (TextView) findViewById(R.id.time_display);
+        timeDisplay.setTextSize(20);
+        timeDisplay.setText("time: ");
+
+        mRequestingLocationUpdates =  true;
+
+        // To bring back saved state if activity is interrupted
+        updateValuesFromBundle(savedInstanceState);
+
+        // Creates Location Request with desired parameters
+        createLocationRequest();
+
         // Used to build the client which allows current location updating
         buildGoogleApiClient();
 
@@ -53,6 +104,13 @@ public class DisplayMapActivity extends FragmentActivity implements GoogleApiCli
 
         // Sets up a GoogleMap and calls onMapReady()
         map.getMapAsync(this);
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -80,11 +138,93 @@ public class DisplayMapActivity extends FragmentActivity implements GoogleApiCli
 
     @Override
     public void onConnected(Bundle connectionHint) {
-        android.location.Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient);
-        if (mLastLocation != null) {
+//        android.location.Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+//                mGoogleApiClient);
+
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+
+//        if (mLastLocation != null) {
             // currentLocationMarker = ourMap.addMarker(new MarkerOptions().title("I'm Here" + iteration).position(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude())));
             // Marker currenLocationMarker = ourMap.addMarker(new MarkerOptions().title("I'm Here").position(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude())));
+//        }
+    }
+
+    protected void startLocationUpdates() {
+        // adds location listener "this" to our Api Client
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    private void updateValuesFromBundle(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            // Update the value of mRequestingLocationUpdates from the Bundle, and
+            // make sure that the Start Updates and Stop Updates buttons are
+            // correctly enabled or disabled.
+            if (savedInstanceState.keySet().contains(REQUESTING_LOCATION_UPDATES_KEY)) {
+                mRequestingLocationUpdates = savedInstanceState.getBoolean(
+                        REQUESTING_LOCATION_UPDATES_KEY);
+                //setButtonsEnabledState();
+            }
+
+            // Update the value of mCurrentLocation from the Bundle and update the
+            // UI to show the correct latitude and longitude.
+            if (savedInstanceState.keySet().contains(LOCATION_KEY)) {
+                // Since LOCATION_KEY was found in the Bundle, we can be sure that
+                // mCurrentLocationis not null.
+                mCurrentLocation = savedInstanceState.getParcelable(LOCATION_KEY);
+            }
+
+            // Update the value of mLastUpdateTime from the Bundle and update the UI.
+            if (savedInstanceState.keySet().contains(LAST_UPDATED_TIME_STRING_KEY)) {
+                mLastUpdateTime = savedInstanceState.getString(
+                        LAST_UPDATED_TIME_STRING_KEY);
+            }
+            updateUI();
+        }
+    }
+
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putBoolean(REQUESTING_LOCATION_UPDATES_KEY,
+                mRequestingLocationUpdates);
+        savedInstanceState.putParcelable(LOCATION_KEY, mCurrentLocation);
+        savedInstanceState.putString(LAST_UPDATED_TIME_STRING_KEY, mLastUpdateTime);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onLocationChanged(android.location.Location location) {
+        mCurrentLocation = location;
+        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
+        updateUI();
+    }
+
+    private void updateUI() {
+        latDisplay.setText(String.valueOf(mCurrentLocation.getLatitude()));
+        lngDisplay.setText(String.valueOf(mCurrentLocation.getLongitude()));
+        timeDisplay.setText(mLastUpdateTime);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // stops location updates when the app is in the background
+        stopLocationUpdates();
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // I am unclear as to why we use not mRequestingLocationUpdates, but I am following Google's
+        // tutorial here
+        if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates) {
+            startLocationUpdates();
         }
     }
 
