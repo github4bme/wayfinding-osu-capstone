3469464c94fdc705c9a39963d7d3fc77544b94c9
joshosu999 <trick.8@osu.edu>
2015-03-09 11:56:48 -0400

    Refactored map display to take out depracated methods.  Began to add GoogleApiClient.

--- app/src/main/java/com/osucse/wayfinding_osu_capstone/DisplayMapActivity.java
+++ app/src/main/java/com/osucse/wayfinding_osu_capstone/DisplayMapActivity.java
@@ -6,15 +6,29 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.LocationServices;
+import android.util.Log;
+import com.google.android.gms.common.ConnectionResult;
+
+
+
+
+
+
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
+//import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolylineOptions;
 import java.util.*;
 
 
-public class DisplayMapActivity extends FragmentActivity {
+public class DisplayMapActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, OnMapReadyCallback {
     private static final LatLng LOWER_MANHATTAN = new LatLng(40.722543,
             -73.998585);
     private static final LatLng TIMES_SQUARE = new LatLng(40.7577, -73.9857);
@@ -37,7 +51,10 @@ public class DisplayMapActivity extends FragmentActivity {
     private static final LatLng NEXT2 = new LatLng(39.9983703, -083.0151790);
     private static final LatLng NEXT3 = new LatLng(39.9983301, -083.0151664);
 
-    private GoogleMap googleMap;
+//    private GoogleMap googleMap;
+
+    protected GoogleMap ourMap;
+    protected GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,10 +71,65 @@ public class DisplayMapActivity extends FragmentActivity {
         startLocationDisplay.setText("starting id: " + startLocation + " ending id:" + endLocation);
         //setContentView(textView);
 
-        setUpMapIfNeeded();
+
+
+
+        buildGoogleApiClient();
+
+        MapFragment map = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        map.getMapAsync(this);
     }
 
-    private void setUpMapIfNeeded() {
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        android.location.Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+                mGoogleApiClient);
+        if (mLastLocation != null) {
+            // currentLocationMarker = ourMap.addMarker(new MarkerOptions().title("I'm Here" + iteration).position(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude())));
+            Marker currenLocationMarker = ourMap.addMarker(new MarkerOptions().title("I'm Here").position(new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude())));
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i("Error:", "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i("Error:", "Connection suspended");
+        mGoogleApiClient.connect();
+    }
+
+//    private void setUpMapIfNeeded() {
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        ourMap = googleMap;
+
         List<LatLng> path = new ArrayList<LatLng>();
         /*path.add(LOWER_MANHATTAN);
         path.add(TIMES_SQUARE);
@@ -68,15 +140,19 @@ public class DisplayMapActivity extends FragmentActivity {
         path.add(NEXT1);
         path.add(NEXT2);
         path.add(NEXT3);
-        // check if we have got the googleMap already
-        if (googleMap == null) {
-            googleMap = ((SupportMapFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.map)).getMap();
-            if (googleMap != null) {
-                addLines(path);
-                googleMap.setMyLocationEnabled(true);
-            }
-        }
+//        // check if we have got the googleMap already
+//        if (googleMap == null) {
+//            googleMap = ((SupportMapFragment) getSupportFragmentManager()
+//                    .findFragmentById(R.id.map)).getMap();
+//            if (googleMap != null) {
+//                addLines(path);
+//                googleMap.setMyLocationEnabled(true);
+//            }
+//        }
+
+        addLines(path);
+        ourMap.setMyLocationEnabled(true);
+        ourMap.addMarker(new MarkerOptions().title("Destination").position(DEST));
     }
 
     private void addLines(List<LatLng> path) {
@@ -92,12 +168,12 @@ public class DisplayMapActivity extends FragmentActivity {
             }
             current++;
             end = path.get(current);
-            googleMap
+            ourMap
                     .addPolyline((new PolylineOptions())
                             .add(start, end).width(5).color(Color.BLUE)
                             .geodesic(true));
             // move camera to zoom on map
-            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(start,
+            ourMap.moveCamera(CameraUpdateFactory.newLatLngZoom(start,
                     18));
         }
     }
