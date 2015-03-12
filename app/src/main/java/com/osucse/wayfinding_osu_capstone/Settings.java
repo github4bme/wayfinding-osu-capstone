5add2dde2d3b93184911e87994a6d6746c3df1ca
Tommy Forte <forte.39@osu.edu>
2015-03-12 15:39:56 -0400

    Added writing and reading to memory operations

--- app/src/main/java/com/osucse/wayfinding_osu_capstone/Settings.java
+++ app/src/main/java/com/osucse/wayfinding_osu_capstone/Settings.java
@@ -1,27 +1,75 @@
 package com.osucse.wayfinding_osu_capstone;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 
 public class Settings extends ActionBarActivity {
+
+    private Switch accessibleSwitch;
+    private Switch visualSwitch;
+    private Switch grailSwitch;
+
+    private SharedPreferences settings;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        // connect private members with view members
+        this.accessibleSwitch = (Switch) findViewById(R.id.settings_switch_accessible);
+        this.visualSwitch = (Switch) findViewById(R.id.settings_switch_visual);
+        this.grailSwitch = (Switch) findViewById(R.id.settings_switch_grail);
+
+        // connect to preferences file
+        settings = getPreferences(MODE_PRIVATE);
+
+        // load switch states from memory
+        this.accessibleSwitch.setChecked(settings.getBoolean("accessible", false));
+        this.visualSwitch.setChecked(settings.getBoolean("visual", false));
+        this.grailSwitch.setChecked(settings.getBoolean("grail", false));
+
+        // for adding commits
+        editor = settings.edit();
     }
 
-    public void accessibleSwitchStateChange () {
 
+    /**
+     * Listener for the accessible switch
+     * @param view
+     */
+    public void accessibleSwitchStateChange (View view) {
+
+        this.editor.putBoolean("accessible", ((Switch) view).isChecked());
+
+        this.editor.commit();
     }
 
-    public void visualSwitchStateChange () {
+    /**
+     * Listener for the visual switch
+     * @param view
+     */
+    public void visualSwitchStateChange (View view) {
 
+        this.editor.putBoolean("visual", ((Switch) view).isChecked());
+
+        this.editor.commit();
     }
 
-    public void grailSwitchStateChange () {
-        
+    /**
+     * Listener for the grail switch
+     * @param view
+     */
+    public void grailSwitchStateChange (View view) {
+
+        this.editor.putBoolean("grail", ((Switch) view).isChecked());
+
+        this.editor.commit();
     }
 
 
