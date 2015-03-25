4185626102400cd3b4c6b952b271f6d56b6ffa0e
joshosu999 <trick.8@osu.edu>
2015-03-25 14:41:19 -0400

    Preliminary changes to bring in directional pointing

--- app/src/main/java/com/osucse/wayfinding_osu_capstone/DisplayMapActivity.java
+++ app/src/main/java/com/osucse/wayfinding_osu_capstone/DisplayMapActivity.java
@@ -6,9 +6,16 @@ import android.os.Bundle;
 import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.widget.TextView;
+
+
+
+
+
+
 import android.util.Log;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.PolylineOptions;
@@ -16,7 +23,7 @@ import java.util.*;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import com.osucse.wayfinding_api.*;
-import com.osucse.utilities.Coordinate;
+//import com.osucse.utilities.Coordinate;
 
 
 public class DisplayMapActivity extends FragmentActivity {
@@ -28,9 +35,34 @@ public class DisplayMapActivity extends FragmentActivity {
 //    private static final LatLng NEXT1 = new LatLng(39.9984717, -083.0151624);
 //    private static final LatLng NEXT2 = new LatLng(39.9983703, -083.0151790);
 //    private static final LatLng NEXT3 = new LatLng(39.9983301, -083.0151664);
+
+
+    /*****
+     * TO BE CHOPPED
+     */
     private static String startLocation = "";
     private static String endLocation = "";
-    private GoogleMap googleMap;
+
+
+
+
+
+
+    private GoogleMap ourMap;
+    private
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,22 +75,31 @@ public class DisplayMapActivity extends FragmentActivity {
         //TextView textView = new TextView(this);
         TextView startLocationDisplay = (TextView) findViewById(R.id.start_location_display);
         startLocationDisplay.setTextSize(20);
-        startLocationDisplay.setText("starting id: " + startLocation + " ending id:" + endLocation);
+        startLocationDisplay.setText("This is all me -- starting id: " + startLocation + " ending id:" + endLocation);
         //setContentView(textView);
         new HttpRequestTask().execute();
-        setUpMapIfNeeded();
+
+
+
+        MapFragment map = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+
+        // Sets up a GoogleMap and calls onMapReady()
+        map.getMapAsync(this);
+
+
+//        setUpMapIfNeeded();
     }
 
-    private void setUpMapIfNeeded() {
-        // check if we have got the googleMap already
-        if (googleMap == null) {
-            googleMap = ((SupportMapFragment) getSupportFragmentManager()
-                    .findFragmentById(R.id.map)).getMap();
-            if (googleMap != null) {
-                googleMap.setMyLocationEnabled(true);
-            }
-        }
-    }
+//    private void setUpMapIfNeeded() {
+//        // check if we have got the googleMap already
+//        if (ourMap == null) {
+//            ourMap = ((SupportMapFragment) getSupportFragmentManager()
+//                    .findFragmentById(R.id.map)).getMap();
+//            if (ourMap != null) {
+//                ourMap.setMyLocationEnabled(true);
+//            }
+//        }
+//    }
 
     private class HttpRequestTask extends AsyncTask<Void, Void, Route> {
         @Override
