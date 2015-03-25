869db4c8fc67b6958b63065652abbfa1eb3755a1
joshosu999 <trick.8@osu.edu>
2015-03-25 16:07:40 -0400

    Direction arrow added to code.  Currently it points to the start location of the route. NOTE: You may need to walk a bit for the 'onLocationChanged' method to be called in order for the arrow to point in the correct direction.

--- app/src/main/java/com/osucse/wayfinding_osu_capstone/DisplayMapActivity.java
+++ app/src/main/java/com/osucse/wayfinding_osu_capstone/DisplayMapActivity.java
@@ -6,12 +6,6 @@ import android.os.Bundle;
 import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.widget.TextView;
-
-
-
-
-
-
 import android.util.Log;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,14 +13,20 @@ import com.google.android.gms.maps.MapFragment;
 
 
 // Josh Added
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.OnMapReadyCallback;
-
-
-
-
-
-
-
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.common.ConnectionResult;
+import android.hardware.SensorEventListener;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.content.Context;
+import android.hardware.SensorEvent;
+import android.widget.ImageView;
 
 
 import com.google.android.gms.maps.model.LatLng;
@@ -35,48 +35,42 @@ import java.util.*;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 import com.osucse.wayfinding_api.*;
-//import com.osucse.utilities.Coordinate;
-//import com.google.android.gms.maps.SupportMapFragment;
 
 
-public class DisplayMapActivity extends FragmentActivity {
 
+public class DisplayMapActivity extends FragmentActivity implements SensorEventListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
     private static final String URL = "http://54.200.238.22:9000/";
-
-//    private static final LatLng DEST = new LatLng(39.9986444, -083.0150867);
-//    private static final LatLng NEXT = new LatLng(39.9985652, -083.0151295);
-//    private static final LatLng NEXT1 = new LatLng(39.9984717, -083.0151624);
-//    private static final LatLng NEXT2 = new LatLng(39.9983703, -083.0151790);
-//    private static final LatLng NEXT3 = new LatLng(39.9983301, -083.0151664);
-
-
-    /*****
-     * TO BE CHOPPED
-     */
-    private static String startLocation = "";
-    private static String endLocation = "";
-
-
-
-
-
+    private static final String LOCATION_KEY = "com.osucse.wayfinding_osu_capstone.DisplayMapActivity.locationKey";
 
     private GoogleMap ourMap;
     List<LatLng> ourRoute = new ArrayList<LatLng>();
 
+    // Used for location services
+    protected GoogleApiClient mGoogleApiClient;
+    protected LocationRequest mLocationRequest;
 
+    // Used for orientation of the phone
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
 
+    protected ImageView arrowImage;
 
+    protected String startLocation;
+    protected String endLocation;
 
+    protected android.location.Location mCurrentLocation;
+    protected float bearingToDestDegrees;
+    protected float currBearing;
 
+    /******
+     *  Boolean value for running on emulator
+     */
+    protected boolean runningOnEmulator = false;
+    /***********/
 
-
-
-
-
-
-
-
+    // Variable that could be used for turning location updates on and off
+    // This might be helpful for running on emulator if we find that the updates cause problems
+    protected boolean mRequestingLocationUpdates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,12 +82,34 @@ public class DisplayMapActivity extends FragmentActivity {
 
         TextView startLocationDisplay = (TextView) findViewById(R.id.start_location_display);
         startLocationDisplay.setTextSize(20);
-        startLocationDisplay.setText("This is all me -- starting id: " + startLocation + " ending id:" + endLocation);
-        //setContentView(textView);
+        startLocationDisplay.setText("This is all me");
+        arrowImage = (ImageView) findViewById(R.id.arrow_image);
+
         new HttpRequestTask().execute();
+
+        // Set to true for all cases because we do not have any reason to turn these updates off
+        // E.g. a setting to disable location information
+        mRequestingLocationUpdates =  true;
+
+        // Creates Location Request with desired parameters
+        createLocationRequest();
+
+        // Used to build the client which allows current location updating
+        buildGoogleApiClient();
+
+        // Set from null to stop initial null pointer exception in onSensorChanged() calling updateUI() before API
+        // client has connected to set our user's current location
+        mCurrentLocation = createAndroidLocation(new LatLng(0.0, 0.0));
+
+        if (!runningOnEmulator) {
+            // Used for getting orientation of phone
+            mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+            mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+        }
+
+        // To bring back saved state if activity is interrupted
+        updateValuesFromBundle(savedInstanceState);
     }
-
-
 
     private class HttpRequestTask extends AsyncTask<Void, Void, Route> implements OnMapReadyCallback {
         @Override
@@ -137,10 +153,9 @@ public class DisplayMapActivity extends FragmentActivity {
 
             plotRoute();
             ourMap.setMyLocationEnabled(true);
+            ourMap.addMarker(new MarkerOptions().title("Start").position(ourRoute.get(0)));
         }
     }
-
-
 
     /**
      * Plots the route on the map
@@ -156,6 +171,180 @@ public class DisplayMapActivity extends FragmentActivity {
             ourMap.addPolyline((new PolylineOptions()).add(ourRoute.get(i), ourRoute.get(i + 1))
                     .width(5).color(Color.BLUE).geodesic(true));
         }
+    }
+
+
+
+
+
+
+
+    /********************************************************************/
+
+
+
+
+
+
+
+    // Defines how and when our location updates are made
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        // Build API Client, add callbacks as needed
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        // I left this commented section out as an example of how to get the user's current location
+        // This could be used when adding the user's current location to the list
+//        android.location.Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
+//                mGoogleApiClient);
+
+        if (mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
+    protected void startLocationUpdates() {
+        // Adds location listener "this" to our Api Client
+        LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);
+    }
+
+    // Code used to repopulate necessary fields if the Activity is interrupted
+    private void updateValuesFromBundle(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            // Update the value of mCurrentLocation from the Bundle
+            if (savedInstanceState.keySet().contains(LOCATION_KEY)) {
+                // Since LOCATION_KEY was found in the Bundle, we can be sure that
+                // mCurrentLocationis not null.
+                mCurrentLocation = savedInstanceState.getParcelable(LOCATION_KEY);
+            }
+            updateUI();
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        // Saves all of the pertinent information if Activity is interrupted
+        savedInstanceState.putParcelable(LOCATION_KEY, mCurrentLocation);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
+    @Override
+    public void onLocationChanged(android.location.Location currentLocation) {
+        // This is called anytime the location is detected as changed
+        mCurrentLocation = currentLocation;
+
+        /***********
+         * logic for location to point arrow at
+         */
+        android.location.Location destLocation = createAndroidLocation(ourRoute.get(0));
+
+        bearingToDestDegrees = mCurrentLocation.bearingTo(destLocation);
+
+        updateUI();
+    }
+
+    private void updateUI() {
+        // Need to rotate the arrow by the difference of the two bearings
+        float arrowRotation = bearingToDestDegrees - currBearing;
+        arrowImage.setRotation(arrowRotation);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Stops location updates when the app is in the background
+        stopLocationUpdates();
+
+        if (!runningOnEmulator) {
+            mSensorManager.unregisterListener(this);
+        }
+    }
+
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // I am unclear as to why we use 'not' mRequestingLocationUpdates, but I am following Google's
+        // tutorial here
+        if (mGoogleApiClient.isConnected() && !mRequestingLocationUpdates) {
+            startLocationUpdates();
+        }
+
+        if (!runningOnEmulator) {
+            mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i("Error:", "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {
+        // The connection to Google Play services was lost for some reason. We call connect() to
+        // attempt to re-establish the connection.
+        Log.i("Error:", "Connection suspended");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // I am unsure why the accuracy would change.  It is possible it is changing in order to suit the
+        // device
+        Log.i("Unknown", "*********************\nAccuracy Changed for Sensor\n*********************");
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // This is called every time a change in the device's orientation is detected
+        // Because this will be called very often, the logic should be kept simple
+
+        // The even.values[2] gives sin(theta/2) where theta is the rotation about the z axis
+        // Need to do this calculation to get the rotation, theta, in degrees
+        currBearing = (float) (Math.toDegrees(Math.asin((double) event.values[2])) * -2.0);
+        updateUI();
+    }
+
+    // Simple helper method for converting from LatLng to an android.location.Location
+    private android.location.Location createAndroidLocation(LatLng point) {
+        // Provider name is unnecessary
+        android.location.Location newLocation = new android.location.Location("");
+        newLocation.setLatitude(point.latitude);
+        newLocation.setLongitude(point.longitude);
+        return newLocation;
     }
 }
 
