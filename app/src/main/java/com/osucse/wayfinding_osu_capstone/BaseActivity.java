fc00af5d52d5e75cc8a526b31917375e56d031b3
Ari Telesman <futbol3737@yahoo.com>
2015-04-22 18:46:43 -0400

    initial drawer skeleton2

--- app/src/main/java/com/osucse/wayfinding_osu_capstone/BaseActivity.java
+++ app/src/main/java/com/osucse/wayfinding_osu_capstone/BaseActivity.java
@@ -1,7 +1,79 @@
 package com.osucse.wayfinding_osu_capstone;
 
+import android.app.Activity;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView.OnItemClickListener;
+import android.view.MenuItem;
+import android.content.res.Configuration;
+
 /**
  * Created by aritelesman on 4/22/15.
  */
-public class BaseActivity {
-}
+public class BaseActivity extends Activity
+{
+    public DrawerLayout drawerLayout;
+    public ListView drawerList;
+    private ActionBarDrawerToggle drawerToggle;
+
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        // R.id.drawer_layout should be in every activity with exactly the same id.
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        drawerToggle = new ActionBarDrawerToggle((Activity) this, drawerLayout, R.drawable.ic_drawer, 0, 0)
+        {
+            public void onDrawerClosed(View view)
+            {
+                getActionBar().setTitle(R.string.app_name);
+            }
+
+            public void onDrawerOpened(View drawerView)
+            {
+                getActionBar().setTitle(R.string.menu);
+            }
+        };
+        drawerLayout.setDrawerListener(drawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        drawerList = (ListView) findViewById(R.id.left_drawer);
+        drawerList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_activated_1,
+                items)); //Items is an ArrayList or array with the items you want to put in the Navigation Drawer.
+
+        drawerList.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int pos, long arg3) {
+                // Do what you want when an item from the Navigation Drawer is clicked
+            }
+
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
