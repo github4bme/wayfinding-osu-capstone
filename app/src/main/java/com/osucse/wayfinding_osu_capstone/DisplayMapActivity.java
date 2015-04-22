49f1ee10c9b5a239cb1707f32e19b2eb71a60a80
Gregg Dent <dent.59@osu.edu>
2015-04-21 20:39:05 -0400

    Hints and Tips Dialog box appears when MapActivity is invoked. It's not pretty or anything special, though.

--- app/src/main/java/com/osucse/wayfinding_osu_capstone/DisplayMapActivity.java
+++ app/src/main/java/com/osucse/wayfinding_osu_capstone/DisplayMapActivity.java
@@ -106,6 +106,8 @@ public class DisplayMapActivity extends FragmentActivity implements SensorEventL
         setContentView(R.layout.activity_display_map);
         Intent intent = getIntent();
 
+        showHintsAndTips();
+
         arrowImage = (ImageView) findViewById(R.id.arrow_image);
         distanceTV = (TextView) findViewById(R.id.distanceTextView);
         etaTV = (TextView) findViewById(R.id.etaTextView);
@@ -173,6 +175,14 @@ public class DisplayMapActivity extends FragmentActivity implements SensorEventL
                 }
             });
         }
+    }
+
+    private void showHintsAndTips() {
+        AlertDialog.Builder hintsAndTipsDialog = new AlertDialog.Builder(DisplayMapActivity.this);
+        hintsAndTipsDialog.setTitle("Hints and Tips");
+        hintsAndTipsDialog.setMessage("The arrow works more efficiently if held parallel to the ground like a compass. Click on the arrow to enlarge it.");
+        hintsAndTipsDialog.setPositiveButton("Close", new DialogInterface.OnClickListener(){@Override public void onClick(DialogInterface dialog, int id){}});
+        hintsAndTipsDialog.show();
     }
 
     private class HttpRequestTask extends AsyncTask<Void, Void, Route> implements OnMapReadyCallback {
